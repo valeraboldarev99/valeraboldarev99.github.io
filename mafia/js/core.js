b6798cf17984156e2046a4f20595127af2acf777
valeriy <work.valeriy99@gmail.com>
2023-12-23 18:42:26 +0600

    sidebar

--- mafia/js/core.js
+++ mafia/js/core.js
@@ -7,33 +7,21 @@ $(document).ready(function () {
 			var self = this;
 			self.init = function()
 			{
-                $('#sidebar-open').on('click', function() {
-                    self.openClose();
-                });
-			},
-			self.openClose = function()
-			{
-                if($('#sidebar-open').hasClass('header__mobile_hamburger'))
-                {
-                    mobileSearch.close();
+                $('#js-sidebar-open').on('click', function() {
                     self.open();
-                }
-                else {
-                   self.close();
-                }
+                });
+                $('#js-sidebar-close').on('click', function() {
+                    self.close();
+                });
 			},
             self.open = function()
             {
                 $('.sidebar').addClass('sidebar_opened');
-                $('#sidebar-open').removeClass('header__mobile_hamburger');     //поменять значек hamburger
-                $('#sidebar-open').addClass('header__mobile_close');            //на значек close
                 $('body').css('overflow', 'hidden');                            //запрет прокрутки
             },
             self.close = function()
             {
                 $('.sidebar').removeClass('sidebar_opened');
-                $('#sidebar-open').removeClass('header__mobile_close');         //поменять значек close
-                $('#sidebar-open').addClass('header__mobile_hamburger');        //на значек hamburger
                 $('body').css('overflow', '');                                  //разрешить прокрутку
             }
 		}
@@ -41,4 +29,28 @@ $(document).ready(function () {
 	}();
 	mobileSidebar.init();
     //end mob menu
+
+    /*sub mob menu*/
+    $(".sidebar__menu_item-set").on("click", function() {
+        var sub_menu = $(this).find('.sidebar__sub-menu_items');
+
+        if(sub_menu.css('display') == 'block')
+        {
+            sub_menu.css('display', 'none');
+            $(this).find('.sidebar__menu_link').css('color', '#fffff');
+            $(this).find('.sidebar__menu_set').css({
+                'transform': 'rotate(-90deg)',
+                'background': 'url("../img/menu_set.svg") center center no-repeat'
+            });
+        }
+        else {
+            sub_menu.css('display', 'block');
+            $(this).find('.sidebar__menu_link').css('color', '#DA2128');
+            $(this).find('.sidebar__menu_set').css({
+                'transform': 'rotate(0deg)',
+                'background': 'url("../img/menu_set.svg") center center no-repeat'
+            });
+        }
+    });
+    /*end sub mob menu*/
 });
